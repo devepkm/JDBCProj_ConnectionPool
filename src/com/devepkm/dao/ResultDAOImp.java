bfb6a9755481b1def6ebb76307a036fc7cf25951
devepkm <devepkm@gmail.com>
2020-09-09 22:13:21 +0800

    finished

--- src/com/devepkm/dao/ResultDAOImp.java
+++ src/com/devepkm/dao/ResultDAOImp.java
@@ -13,19 +13,36 @@ import java.sql.SQLException;
 public class ResultDAOImp {
 
 
-    private BeanHandler<Result> handler;
-
     public Result getResult(Connection conn, String sql, Object... args) {
 
         QueryRunner runner = new QueryRunner();
-        BeanHandler<Result>handler = new BeanHandler<>(Result.class);
+        BeanHandler<Result> handler = new BeanHandler<>(Result.class);
         Result rs = null;
         try {
-            rs = runner.query(JDBCUtils.getConnection(), sql, handler,args);
+            rs = runner.query(JDBCUtils.getConnection(), sql, handler, args);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
         return rs;
 
     }
+
+    public void modifyResult(Connection conn, String hkid, Result rs) {
+        String sql = "UPDATE result SET chinese = ?, english = ?, maths = ?, LiberalStudies = ?  WHERE hkid = ?;";
+        Object[] args = new Object[5];
+        args[0] = rs.getChinese();
+        args[1] = rs.getEnglish();
+        args[2] = rs.getMaths();
+        args[3] = rs.getLiberalStudies();
+        args[4] = hkid;
+        QueryRunner runner = new QueryRunner();
+        try {
+            runner.update(conn,sql,args);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
+
+
 }
